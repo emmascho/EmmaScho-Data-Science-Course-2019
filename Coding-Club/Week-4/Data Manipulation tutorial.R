33da9be1ae2f9e30ac8917b8b51e280c3b064c62
emmascho <s1754286@sms.ed.ac.uk>
2019-10-09 16:59:32 +0100

    Did coding club tutorial

--- Coding-Club/Week-4/Data Manipulation tutorial.R
+++ Coding-Club/Week-4/Data Manipulation tutorial.R
@@ -1,1 +1,122 @@
-nh
+setwd("C:/Users/Emma Schoenmakers/Documents/EmmaScho-Data-Science-Course-2019/Coding-Club/Week-4/CC-3-DataManip-master/CC-3-DataManip-master")
+
+# Load the elongation data
+elongation <- read.csv("EmpetrumElongation.csv", header = TRUE)
+
+# Check import and preview data
+head(elongation)   # first few observations
+str(elongation)    # types of variables
+
+# Here's how we get the value in the second row and fifth column
+elongation[2,5]
+
+# Here's how we get all the info for row number 6
+elongation[6, ]
+
+# And of course you can mix it all together! 
+elongation[6, ]$Indiv   # returns the value in the column Indiv for the sixth observation
+# (much easier calling columns by their names than figuring out where they are!) 
+
+# Subsetting with one condition
+elongation[elongation$Zone < 4, ]    # returns only the data for zones 2-3
+elongation[elongation$Zone <= 4, ]   # returns only the data for zones 2-3-4
+
+# This is completely equivalent to the last statement
+elongation[!elongation$Zone > 5, ]   # the ! means exclude
+
+# Subsetting with two conditions
+elongation[elongation$Zone == 2 | elongation$Zone == 7, ]    # returns only data for zones 2 and 7
+
+elongation[elongation$Zone == 2 & elongation$Indiv %in% c(300:400), ]    # returns data for shrubs in zone 2 whose ID numbers are between 300 and 400
+
+rep(seq(0, 30, 10), 4)
+
+# Let's create a working copy of our object
+elong2 <- elongation
+
+names(elong2)                 # returns the names of the columns
+names(elong2)[1] <- "zone"    # Changing Zone to zone: we call the 1st element of the names vector using brackets, and assign it a new value
+names(elong2)[2] <- "ID"      # Changing Indiv to ID: we call the 2nd element and assign it the desired value
+## - option 1: you can use row and column number
+elong2[1,4] <- 5.7
+elong2[elong2$ID == 373, ]$X2008 <- 5.7   # completely equivalent to option 1
+
+## CREATING A FACTOR
+
+# Let's check the classes 
+str(elong2)
+
+elong2$zone <- as.factor(elong2$zone)        # converting and overwriting original class
+str(elong2)                                  # now zone is a factor with 6 levels
+
+## CHANGING A FACTOR'S LEVELS
+
+levels(elong2$zone)  # shows the different factor levels
+
+levels(elong2$zone) <- c("A", "B", "C", "D", "E", "F")   # you can overwrite the original levels with new names
+
+# You must make sure that you have a vector the same length as the number of factors, and pay attention to the order in which they appear!
+install.packages("tidyr")  # install the package
+library(tidyr)             # load the package
+
+elongation_long <- gather(elongation, Year, Length,                           # in this order: data frame, key, value
+                          c(X2007, X2008, X2009, X2010, X2011, X2012))        # we need to specify which columns to gather
+# Let's reverse! spread() is the inverse function, allowing you to go from long to wide format
+elongation_wide <- spread(elongation_long, Year, Length)
+
+elongation_long2 <- gather(elongation, Year, Length, c(3:8))
+
+
+boxplot(Length ~ Year, data = elongation_long, 
+        xlab = "Year", ylab = "Elongation (cm)", 
+        main = "Annual growth of Empetrum hermaphroditum")
+
+install.packages("dplyr")  # install the package
+library(dplyr)              # load the package
+
+elongation_long <- rename(elongation_long, zone = Zone, indiv = Indiv, year = Year, length = Length)     # changes the names of the columns (getting rid of capital letters) and overwriting our data frame
+# As we saw earlier, the base R equivalent would have been
+names(elongation_long) <- c("zone", "indiv", "year", "length")
+
+# FILTER OBSERVATIONS
+
+# Let's keep observations from zones 2 and 3 only, and from years 2009 to 2011
+
+elong_subset <- filter(elongation_long, zone %in% c(2, 3), year %in% c("X2009", "X2010", "X2011")) # you can use multiple different conditions separated by commas
+# For comparison, the base R equivalent would be (not assigned to an object here):
+elongation_long[elongation_long$zone %in% c(2,3) & elongation_long$year %in% c("X2009", "X2010", "X2011"), ]
+
+# SELECT COLUMNS
+
+# Let's ditch the zone column just as an example
+
+elong_no.zone <- dplyr::select(elongation_long, indiv, year, length)   # or alternatively
+elong_no.zone <- dplyr::select(elongation_long, -zone) # the minus sign removes the column
+
+# For comparison, the base R equivalent would be (not assigned to an object here):
+elongation_long[ , -1]  # removes first column
+
+# A nice hack! select() lets you rename and reorder columns on the fly
+elong_no.zone <- dplyr::select(elongation_long, Year = year, Shrub.ID = indiv, Growth = length)
+
+# CREATE A NEW COLUMN 
+
+elong_total <- mutate(elongation, total.growth = X2007 + X2008 + X2009 + X2010 + X2011 + X2012)
+
+# GROUP DATA
+
+elong_grouped <- group_by(elongation_long, indiv)   # grouping our dataset by individual
+
+# SUMMARISING OUR DATA
+
+summary1 <- summarise(elongation_long, total.growth = sum(length))
+summary2 <- summarise(elong_grouped, total.growth = sum(length))
+summary3 <- summarise(elong_grouped, total.growth = sum(length),
+                      mean.growth = mean(length),
+                      sd.growth = sd(length))
+# Load the treatments associated with each individual
+
+treatments <- read.csv("EmpetrumTreatments.csv", header = TRUE, sep = ";")
+head(treatments)
+
+
